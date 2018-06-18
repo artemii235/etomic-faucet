c1870eb9175eb50d0c51f2cb3b16d23677c17cd8
Artem Pikulin <ortgma@gmail.com>
2018-06-18 13:16:44 +0700

    Send up to 100 ETOMIC as only 1 ETOMIC is required for ETH/ERC20 trades.

--- src/faucet.js
+++ src/faucet.js
@@ -1,8 +1,4 @@
-const Web3 = require('web3');
-const config = require('./config');
-const web3 = new Web3(process.env.ETH_RPC_URL);
 const request = require('request-promise-native');
-const BN = require('bignumber.js');
 const ElectrumCli = require('electrum-client');
 
 async function getEtomicBalance(address) {
@@ -35,46 +31,21 @@ function sendEtomicToAddress(address, amount) {
   return request(options);
 }
 
-function ethplorerAddrInfo(address) {
-  const options = {
-    method: 'GET',
-    uri: `https://api.ethplorer.io/getAddressInfo/${ address }?apiKey=freekey`,
-    json: true
-  };
-  return request(options);
-}
-
 async function faucet(req, res) {
   try {
-    let addrInfo = await ethplorerAddrInfo(req.body.ethAddress);
-    let totalBalance = new BN(web3.utils.toWei(addrInfo.ETH.balance.toString()));
-    if (addrInfo.tokens) {
-      for (let token of addrInfo.tokens) {
-        let tokenBalance = new BN(token.balance);
-        if (tokenBalance.gt(0)) {
-          let decimals = Number(token.tokenInfo.decimals);
-          if (decimals < 18) {
-            const multiply = new BN(10).pow(18 - decimals);
-            tokenBalance = tokenBalance.times(multiply);
-          }
-          totalBalance = totalBalance.plus(tokenBalance);
-        }
-      }
-    }
-
-    const toSend = totalBalance.times(9).div(4).div(new BN(10).pow(18));
+    const toSend = 100;
     const etomicBalanceObject = await getEtomicBalance(req.body.etomicAddress);
     const requiredEtomic = toSend - (etomicBalanceObject.confirmed + etomicBalanceObject.unconfirmed) / 100000000;
 
     let txIds = [];
     let etomicPart = (requiredEtomic / 3).toFixed(8);
-    if (etomicPart > 0) {
+    if (etomicPart >= 10) {
       for (let i = 0; i < 3; i++) {
         txIds.push((await sendEtomicToAddress(req.body.etomicAddress, etomicPart)).result);
       }
       res.json({result: txIds});
     } else {
-      res.json({result: "ETH/ERC20 balance is zero or address already has enough ETOMIC"});
+      res.json({result: "Address already has enough ETOMIC"});
     }
   } catch (e) {
     console.log(e);
